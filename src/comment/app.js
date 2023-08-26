77ab15103ebce92fd6addfa61a865aae86ee7f0a
tkskdldnelfm <kakd1313@naver.com>
2023-08-26 19:53:42 +0900

    get test

--- src/comment/app.js
+++ src/comment/app.js
@@ -50,25 +50,25 @@ app.get('/users/:id', cors(corsOptions), function (req, res, next) {
   res.json({ msg: 'https://web-bu-web-exhibition-fq2r52kllqhhlnh.sel3.cloudtype.app 규칙인 Origin에 대하여 개방' })
 })
 
-
-
-
 // 댓글 생성 API
 app.post('/comments', async (req, res) => {
   try {
     const { target_id, nickname, password, comment } = req.body;
     const created_at = new Date();
 
-    const connection = await pool.getConnection();
     const query = `
       INSERT INTO comments (target_id, nickname, password, comment, created_at)
       VALUES (?, ?, ?, ?, ?)
     `;
 
-    await connection.query(query, [target_id, nickname, password, comment, created_at]);
-    connection.release();
-
-    res.json({ message: '댓글이 생성되었습니다.' });
+    connection.query(query, [target_id, nickname, password, comment, created_at], (err) => {
+      if (err) {
+        console.error('댓글 생성 오류:', err);
+        res.status(500).json({ message: '댓글 생성에 실패했습니다.' });
+      } else {
+        res.json({ message: '댓글이 생성되었습니다.' });
+      }
+    });
   } catch (error) {
     console.error('댓글 생성 오류:', error);
     res.status(500).json({ message: '댓글 생성에 실패했습니다.' });
@@ -80,37 +80,38 @@ app.get('/comments/:target_id', async (req, res) => {
   try {
     const target_id = req.params.target_id;
 
-    const connection = await pool.getConnection();
     const query = 'SELECT * FROM comments WHERE target_id = ?';
 
-    const [rows] = await connection.query(query, [target_id]);
-    connection.release();
-
-    res.json(rows);
+    connection.query(query, [target_id], (err, rows) => {
+      if (err) {
+        console.error('댓글 조회 오류:', err);
+        res.status(500).json({ message: '댓글 조회에 실패했습니다.' });
+      } else {
+        res.json(rows);
+      }
+    });
   } catch (error) {
     console.error('댓글 조회 오류:', error);
     res.status(500).json({ message: '댓글 조회에 실패했습니다.' });
   }
 });
 
-
-
-
 // /users 경로로 GET 요청 처리
 app.get('/users', async (req, res) => {
-  try {
-    const connection = await pool.getConnection();
-    const sql = 'SELECT * FROM users'; // users 테이블의 모든 열을 선택하는 SQL 쿼리
+  const sql = 'SELECT * FROM users'; // users 테이블의 모든 열을 선택하는 SQL 쿼리
 
-    const [results] = await connection.query(sql);
-    connection.release();
-
-    res.json(results);
-  } catch (err) {
-    console.error('Error executing query:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json(results);
+    }
+  });
 });
+
+
+
 
 
 
