187ca76c5ab9537f4b592bd4390413465e12c47d
tkskdldnelfm <kakd1313@naver.com>
2023-08-26 20:06:41 +0900

    test

--- src/comment/app.js
+++ src/comment/app.js
@@ -46,6 +46,22 @@ connection.connect((err) => {
 });
 
 
+// /users 경로로 GET 요청 처리
+app.get('/users', (req, res) => {
+  const sql = 'SELECT * FROM users'; // users 테이블의 모든 열을 선택하는 SQL 쿼리
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    res.json(results); // 결과를 JSON 형태로 응답
+    console.log('done');
+  });
+});
+
+
 app.get('/users/:id', cors(corsOptions), function (req, res, next) {
   res.json({ msg: 'https://web-bu-web-exhibition-fq2r52kllqhhlnh.sel3.cloudtype.app 규칙인 Origin에 대하여 개방' })
 })
